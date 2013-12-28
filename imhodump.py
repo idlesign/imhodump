7b653380704506079629af0385aa696e97a44a8d
idle sign <idlesign@yandex.ru>
2013-12-28 17:36:17 +0700

    Немного автоматизации для последующей [ручной] работы.

--- imhodump.py
+++ imhodump.py
@@ -4,164 +4,274 @@ import logging
 import os
 import shutil
 import datetime
+import argparse
 
 from lxml import etree
 from json import dumps, loads
+from urllib.parse import quote
+from math import ceil
 
 
 logging.basicConfig()
 logger = logging.getLogger(os.path.basename(__file__))
 logger.setLevel(logging.INFO)
 
-
-USERNAME = 'idle'
-
-SUBJECT = 'films'
-#SUBJECT = 'books'
-#SUBJECT = 'games'
-#SUBJECT = 'serials'
+VERSION = (0, 4, 0)
 
 
-VERSION = (0, 3, 0)
+class ImhoDumper():
 
-URL_RATES_TPL = 'http://%s.imhonet.ru/content/%s/rates/%s/'
-OUTPUT_FILENAME = 'imho_rates_%s_%s.json' % (SUBJECT, USERNAME)
-START_FROM_RATING = 1
+    SUBJECT_FILMS = 'films'
+    SUBJECT_BOOKS = 'books'
+    SUBJECT_GAMES = 'games'
+    SUBJECT_SERIES = 'serials'
 
+    TARGET_GOODREADS = 'Goodreads'
+    TARGET_KINOPOISK = 'КиноПоиск'
+    TARGETS = {
+        TARGET_GOODREADS: 'https://www.goodreads.com/search?utf8=%E2%9C%93&q={term}&search_type=books',
+        TARGET_KINOPOISK: 'http://www.kinopoisk.ru/index.php?first=no&what=&kp_query={term}',
+    }
 
-def get_rates(html, rating):
-    rate_boxes = html.xpath("//div[@class='m-inlineitemslist-describe']")
-    for rate_box in rate_boxes:
-        heading = rate_box.xpath("div[@class='m-inlineitemslist-describe-h2']/a")[0]
-        title_ru = heading.text.strip()
-        details_url = heading.get('href').strip()
-        logger.info('Обрабатываем "%s" ...' % title_ru)
+    SUBJECTS = {
+        SUBJECT_FILMS: [TARGET_KINOPOISK],
+        SUBJECT_BOOKS: [TARGET_GOODREADS],
+        SUBJECT_GAMES: [],
+        SUBJECT_SERIES: [TARGET_KINOPOISK]
+    }
 
-        info = rate_box.xpath("div[@class='m-inlineitemslist-describe-gray']")[0].text.strip()
+    URL_RATES_TPL = 'http://%s.imhonet.ru/content/%s/rates/%s/'
+    START_FROM_RATING = 1
 
-        req_details = requests.get(details_url)
-        html_details = etree.HTML(req_details.text)
+    def __init__(self, username, subject):
+        self.username = username
+        self.subject = subject
+        self.output_filename = 'imho_rates_%s_%s.json' % (subject, username)
+
+    def get_rates(self, html, rating):
+        rate_boxes = html.xpath("//div[@class='m-inlineitemslist-describe']")
+        for rate_box in rate_boxes:
+            heading = rate_box.xpath("div[@class='m-inlineitemslist-describe-h2']/a")[0]
+            title_ru = heading.text.strip()
+            details_url = heading.get('href').strip()
+            logger.info('Обрабатываем "%s" ...' % title_ru)
+
+            info = rate_box.xpath("div[@class='m-inlineitemslist-describe-gray']")[0].text.strip()
+
+            req_details = requests.get(details_url)
+            html_details = etree.HTML(req_details.text)
+
+            try:
+                title_orig = html_details.xpath("//div[@class='m-elementprimary-language']")[0].text.strip()
+            except (IndexError, AttributeError):
+                logger.debug('** Название на языке оригинала не заявлено, наверное наше кино')
+                title_orig = None
+
+            logger.debug('Оригинальное название: %s' % title_orig)
+
+            try:
+                year = info.split('<br>')[0].strip().split(',')[-1].strip().split(' ')[0].strip()
+            except AttributeError:
+                year = None
+
+            logger.debug('Год: %s' % year)
+
+            if year is not None:
+                title_ru = title_ru.replace('(%s)' % year, '').strip()
+
+            item_data = {
+                'title_ru': title_ru,
+                'title_orig': title_orig,
+                'rating': rating,
+                'year': year,
+                'details_url': details_url
+            }
+
+            yield item_data
+
+    def process_url(self, page_url, rating, recursive=False):
+
+        logger.info('Обрабатывается страница %s ...' % page_url)
+        logger.debug('Рейтинг: %s' % rating)
+
+        req = requests.get(page_url)
+        text = req.text.replace('<!--noindex-->', ''). replace('<!--/noindex-->', '')
+        html = etree.HTML(text)
 
         try:
-            title_orig = html_details.xpath("//div[@class='m-elementprimary-language']")[0].text.strip()
-        except (IndexError, AttributeError):
-            logger.debug('** Название на языке оригинала не заявлено, наверное наше кино')
-            title_orig = None
+            next_page_url = html.xpath("//div[@class='m-pagination']/a")[-1].get('href')
+        except IndexError:
+            next_page_url = None
 
-        logger.debug('Оригинальное название: %s' % title_orig)
+        if next_page_url == page_url:
+            next_page_url = None
 
-        try:
-            year = info.split('<br>')[0].strip().split(',')[-1].strip().split(' ')[0].strip()
-        except AttributeError:
-            year = None
+        logger.info('Следующая страница: %s' % next_page_url)
 
-        logger.debug('Год: %s' % year)
+        yield from self.get_rates(html, rating)
 
-        if year is not None:
-            title_ru = title_ru.replace('(%s)' % year, '').strip()
+        if recursive and next_page_url is not None:
+            yield from self.process_url(next_page_url, rating, recursive)
 
-        item_data = {
-            'title_ru': title_ru,
-            'title_orig': title_orig,
-            'rating': rating,
-            'year': year,
-            'details_url': details_url
-        }
+    def dump_to_file(self, filename, existing_items=None, start_from_rating=1):
+        logger.info('Собираем оценки пользователя %s в файл %s' % (self.username, filename))
 
-        yield item_data
+        with open(filename, 'w') as f:
+            f.write('[')
+            try:
+                if existing_items:
+                    f.write('%s,' % dumps(list(existing_items.values()), indent=4).strip('[]'))
+                for rating in range(start_from_rating, 10):
+                    for item_data in self.process_url(self.URL_RATES_TPL % (self.username, self.subject, rating), rating, True):
+                        if item_data['details_url'] not in existing_items:
+                            f.write('%s,' % dumps(item_data, indent=4))
+            finally:
+                f.write('{}]')
 
+    def load_from_file(self, filename):
+        result = {}
+        if os.path.exists(filename):
+            logger.info('Загружаем ранее собранные оценки пользователя %s из файла %s' % (self.username, filename))
+            with open(filename, 'r') as f:
+                data = f.read()
+            result = {entry['details_url']: entry for entry in loads(data) if entry}
+        return result
 
-def process_url(page_url, rating, recursive=False):
+    def make_html(self, filename):
 
-    logger.info('Обрабатывается страница %s ...' % page_url)
-    logger.debug('Рейтинг: %s' % rating)
+        html_base = '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Оценки подраздела %(subject)s imhonet</title>
+            <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+            <style>
+                body {
+                    color: #333;
+                    font-family: Verdana, Arial, helvetica, sans-serif;
+                }
+                h1, h6 {
+                    color: #999;
+                }
+                .rate_block {
+                    border-bottom: 1px solid #eee;
+                    padding: 0.4em;
+                    padding-bottom: 1.2em;
+                }
+                .rating {
+                    font-size: 1.5em;
+                }
+                .info, .description {
+                    display: inline-block;
+                    margin-left: 0.7em;
+                    vertical-align: middle;
+                }
+                .rating .current {
+                    color: #800;
+                }
+                .rating .total {
+                    font-size: 0.7em;
+                    color: #aaa;
+                }
+                .title_ru {
+                    font-size: 1.7em;
+                }
+                .title_orig {
+                    color: #aaa;
+                }
+                .links {
+                    padding-top: 0.5em;
+                    font-size: 0.8em;
+                }
+                .link {
+                    display: inline-block;
+                    margin-right: 0.5em;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Оценки подраздела %(subject)s imhonet</h1>
+            <h6>Всего оценок: %(rates_num)s</h6>
+            %(rating_rows)s
+        </body>
+        </html>
+        '''
 
-    req = requests.get(page_url)
-    text = req.text.replace('<!--noindex-->', ''). replace('<!--/noindex-->', '')
-    html = etree.HTML(text)
+        html_rating_row = '''
+        <div class="rate_block">
+            <div class="info">
+                <div class="year">%(year)s</div>
+                <div class="rating">
+                    <span class="current">%(rating)s</span><span class="total">/10</span>
+                    <span class="current">%(rating_five)s</span><span class="total">/5</span>
+                </div>
+            </div>
+            <div class="description">
+                <div class="titles">
+                    <div class="title_ru">
+                        <label><input type="checkbox"> %(title_ru)s</label>
+                    </div>
+                    <div class="title_orig">%(title_orig)s</div>
+                </div>
+                <div class="links">
+                    Поиск:
+                    %(links)s
+                </div>
+            </div>
+        </div>
+        '''
 
-    try:
-        next_page_url = html.xpath("//div[@class='m-pagination']/a")[-1].get('href')
-    except IndexError:
-        next_page_url = None
+        html_link_row = '''
+        <div class="link"><a href="%(link)s" target="_blank">%(title)s</a></div>
+        '''
 
-    if next_page_url == page_url:
-        next_page_url = None
+        records = self.load_from_file(filename)
 
-    logger.info('Следующая страница: %s' % next_page_url)
+        rating_rows = []
+        for record in records.values():
+            links = []
+            for link_type in self.SUBJECTS[self.subject]:
+                for title_type in ('title_orig', 'title_ru'):
+                    if record[title_type]:
+                        links.append(html_link_row % {
+                            'link': self.TARGETS[link_type].replace('{term}', quote(record[title_type])),
+                            'title': '%s (%s)' % (link_type, title_type)
+                        })
 
-    yield from get_rates(html, rating)
+            record['links'] = '\n'.join(links)
+            record['rating_five'] = ceil(record['rating'] / 2)
+            del record['details_url']
+            rating_rows.append(html_rating_row % record)
 
-    if recursive and next_page_url is not None:
-        yield from process_url(next_page_url, rating, recursive)
+        target_file = '%s.html' % os.path.splitext(filename)[0]
+        logger.info('Создаём html файл с оценками: %s' % target_file)
+        with open(target_file, 'w') as f:
+            f.write(html_base % {'subject': self.subject, 'rates_num': len(records), 'rating_rows': '\n'.join(rating_rows)})
 
+    def backup_json(self, filename):
+        target_filename = '%s.bak%s' % (filename, datetime.datetime.isoformat(datetime.datetime.now()))
+        logger.info('Делаем резервную копию файла с оценками: %s' % target_filename)
+        shutil.copy(filename, target_filename)
 
-def dump_to_file(filename, existing_items=None, start_from_rating=1):
-    logger.info('Собираем оценки пользователя %s в файл %s' % (USERNAME, filename))
-
-    with open(filename, 'w') as f:
-        f.write('[')
-        try:
-            if existing_items:
-                f.write('%s,' % dumps(list(existing_items.values()), indent=4).strip('[]'))
-            for rating in range(start_from_rating, 10):
-                for item_data in process_url(URL_RATES_TPL % (USERNAME, SUBJECT, rating), rating, True):
-                    if item_data['details_url'] not in existing_items:
-                        f.write('%s,' % dumps(item_data, indent=4))
-        finally:
-            f.write('{}]')
-
-
-def load_from_file(filename):
-    result = {}
-    if os.path.exists(filename):
-        logger.info('Загружаем ранее собранные оценки пользователя %s из файла %s' % (USERNAME, filename))
-        with open(filename, 'r') as f:
-            data = f.read()
-        result = {entry['details_url']: entry for entry in loads(data) if entry}
-    return result
-
-
-def backup_json(filename):
-    target_filename = '%s.bak%s' % (filename, datetime.datetime.isoformat(datetime.datetime.now()))
-    logger.info('Делаем резервную копию файла с оценками: %s' % target_filename)
-    shutil.copy(filename, target_filename)
-
-
-def json_to_imdb_csv(input_filename):
-    target_filename = '%s.csv' % os.path.splitext(os.path.basename(input_filename))[0]
-    logger.info('Экспортируем оценки из файла %s в файл %s' % (input_filename, target_filename))
-    items = load_from_file(input_filename)
-    out_lines = [
-        '"position","const","created","modified","description","Title","Title type","Directors","You rated","IMDb Rating","Runtime (mins)","Year","Genres","Num. Votes","Release Date (month/day/year)","URL"',
-    ]
-    for position, item in enumerate(items.values(), 1):
-        line = '"%(position)s","%(id)s","%(created)s","%(modified)s","%(description)s","%(title)s","%(type)s","%(director)s","%(rating)s","%(rating_imdb)s","%(runtime)s","%(year)s","%(genres)s","%(votes)s","%(release_date)s","%(url)s"' % {
-            'position': position,
-            'id': 'tt0000000',  # По этому идентификатору КиноПоиск производит связь. А у нас он фиктивный %(
-            'created': 'Wed Oct 16 00:00:00 2013',
-            'modified': '',
-            'description': '',
-            'title': item['title_ru'].replace('"', '""'),
-            'type': '',
-            'director': '',
-            'rating': item['rating'],
-            'rating_imdb': '0.0',
-            'runtime': '0',
-            'year': item['year'],
-            'genres': '',
-            'votes': '0',
-            'release_date': '',
-            'url': item['details_url']
-        }
-        out_lines.append(line)
-    with open(target_filename, 'w') as f:
-        f.write('\n'.join(out_lines))
+    def dump(self):
+        existing_items = self.load_from_file(self.output_filename)
+        if existing_items:
+            self.backup_json(self.output_filename)
+        self.dump_to_file(self.output_filename, existing_items=existing_items, start_from_rating=self.START_FROM_RATING)
+        self.make_html(self.output_filename)
 
 
 if __name__ == '__main__':
-    existing_items = load_from_file(OUTPUT_FILENAME)
-    if existing_items:
-        backup_json(OUTPUT_FILENAME)
-    dump_to_file(OUTPUT_FILENAME, existing_items=existing_items, start_from_rating=START_FROM_RATING)
-    json_to_imdb_csv(OUTPUT_FILENAME)
+
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('username', help='Имя пользователя imhonet')
+    args_parser.add_argument('subject', help='Категория: %s' % ', '.join([s for s in ImhoDumper.SUBJECTS.keys()]))
+    args_parser.add_argument('--html_only', help='Указывает, что требуется только экспорт уже имеющегося файла с оценками в html', action='store_true')
+
+    parsed = args_parser.parse_args()
+
+    dumper = ImhoDumper(parsed.username, parsed.subject)
+    if parsed.html_only:
+        dumper.make_html(dumper.output_filename)
+    else:
+        dumper.dump()
